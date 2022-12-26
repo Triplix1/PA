d51d5af50bf0240a39fed11f030786dc4511b6b6
Triplix1 <vladikwww3vladik@gmail.com>
2022-12-26 14:37:54 +0200

    Add Lab4

---
+++ Lab 4/Program.cs
@@ -0,0 +1,38 @@
+﻿using Lab_4;
+
+int populationNum = 100;
+int elementsNum = 100;
+int minWeight = 1;
+int maxWeight = 10;
+int minCost = 2;
+int maxCost = 20;
+int limit = 250;
+double mutationChance = 0.05;
+int point = 50;
+int iterations = 1000;
+
+var population = Generator.GeneratePopulation(populationNum,elementsNum);
+
+var (cost, weight) = Generator.GenerateCostWeight(elementsNum, minWeight, maxWeight, minCost, maxCost);
+
+var genetic = new Genetic(cost.ToArray(), weight.ToArray(), (Member[])population.Clone(), limit, mutationChance, point, iterations);
+
+var (current, Maxcost, Maxweight) = genetic.Run();
+
+Console.WriteLine("Generated Cost-Weight:\n");
+
+PrintCostAndWeight(cost, weight);
+
+Console.WriteLine("\n-----------\n");
+
+Console.WriteLine("Resulted Cnapsack:");
+Console.WriteLine("Cost: " + Maxcost.ToString());
+Console.WriteLine("Weight: " + Maxweight.ToString());
+
+void PrintCostAndWeight(int[] cost,int[] weight)
+{
+    for (int i = 0; i < cost.Length; i += 2)
+    {
+        Console.WriteLine($"{cost[i].ToString(),-3}" + "- " + $"{weight[i].ToString(),-3}" + "  " + $"{cost[i + 1].ToString(),-3}" + "- " + $"{weight[i + 1].ToString(),-3}");
+    }
+}
---
+++ Lab 4/WorkWithList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    public class WorkWithList
+    {
+        public static void Add<T>(List<T> arr, T node, Func<T, int> func)
+        {
+            if (arr is null || node is null || func is null)
+            {
+                throw new ArgumentNullException("Parameters can't be null");
+            }
+            int start = 0;
+            int end = arr.Count - 1;
+
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+
+                if (func(arr[mid]) == func(node))
+                {
+                    arr.Insert(mid, node);
+                    return;
+                }
+                else if (func(arr[mid]) < func(node))
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            arr.Insert(end + 1, node);
+        }
+    }
+}
